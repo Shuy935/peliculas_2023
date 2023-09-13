b1bea7c073573e1449ba5ee0a5fab8f1c7199439
Shuy935 <jessus_2500@hotmail.com>
2023-09-12 22:08:59 -0600

    Primer comit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas_2023/screens/screens.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pelicula',
+      initialRoute: 'home',
+      routes: {
+        'home': (_) => HomeScreen(),
+        'details': (context) => DetailsScreen(), 
+      },
+    );
+  }
+}
---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,1 @@
+export 'package:peliculas_2023/widgets/Card_Swiper.dart';
