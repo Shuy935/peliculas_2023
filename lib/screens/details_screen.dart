6f75ae8805828de803aa799c42cc84160c7b52ae
Shuy935 <jessus_2500@hotmail.com>
2023-09-20 11:32:28 -0600

    Sliders

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -5,9 +5,39 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //recibir argumentos de otra pantalla
+    final String movie = ModalRoute.of(context)?.settings.arguments.toString() ?? 'Sin nombre';
     return const Scaffold(
-      body: Text('Details Screen'),
+      body: CustomScrollView(
+        slivers: [_CustomAppBar()],
+      ),
     );
     
+  }
+}
+class _CustomAppBar extends StatelessWidget {
+  const _CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.black,
+      expandedHeight: 200,
+      floating: false,
+      //TODO: Que no desaparezca
+      //WIDGET para que se ajuste al tamaño
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black12,
+          child: Text('Movie.Title', style: TextStyle(fontSize: 18),
+          ),
+        ),
+        background: FadeInImage(placeholder: AssetImage('assets/loading.gif'), image: AssetImage('assets/foto.jpeg')),
+      ),
+    );
   }
 }
