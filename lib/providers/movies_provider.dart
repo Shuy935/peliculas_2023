886f87b294879ac6afd51e0fd393f67b3231eab1
Shuy935 <jessus_2500@hotmail.com>
2023-09-28 11:25:09 -0600

    Movie_provider a medias

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,3 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 
+class MovieProvider extends ChangeNotifier {
+  String _baseUrl = 'api.themoviedb.org';
+  String _apiKey = '9dc27117b000e7e5acfb365fa957971a';
+  String _lenguaje = 'es-MX';
+
+  MoviesProvider() {
+    getOnDispayMovies();
+  }
+
+  getOnDispayMovies() {
+    var url = Uri.https(_baseUrl, '3/movie/now_playing', {
+      'api_key': _apiKey,
+      'lenguaje': _lenguaje,
+      'page': '1',
+    });
+  }
+}
