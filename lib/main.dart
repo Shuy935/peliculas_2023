0a1c25c8a9f67e756519ff3576da9a99632bfba6
Shuy935 <jessus_2500@hotmail.com>
2023-09-28 11:51:30 -0600

    Peticion a servidor

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:peliculas_2023/screens/screens.dart';
 import 'package:provider/provider.dart';
+import 'providers/movies_provider.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const AppState());
 
 class AppState extends StatelessWidget {
   const AppState({super.key});
--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,7 +1,9 @@
-import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'dart:convert';
 
-class MovieProvider extends ChangeNotifier {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class MoviesProvider extends ChangeNotifier {
   String _baseUrl = 'api.themoviedb.org';
   String _apiKey = '9dc27117b000e7e5acfb365fa957971a';
   String _lenguaje = 'es-MX';
@@ -10,11 +12,15 @@ class MovieProvider extends ChangeNotifier {
     getOnDispayMovies();
   }
 
-  getOnDispayMovies() {
+  getOnDispayMovies() async{
     var url = Uri.https(_baseUrl, '3/movie/now_playing', {
       'api_key': _apiKey,
-      'lenguaje': _lenguaje,
+      'language': _lenguaje,
       'page': '1',
     });
+    final response = await http.get(url);
+    final Map<String, dynamic> decodeData = json.decode(response.body);
+    print(decodeData);
+    print(response.body);
   }
 }
